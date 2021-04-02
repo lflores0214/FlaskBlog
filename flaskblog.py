2142b274331ee29a748d9355226859d4ad483af7
Luis <lflores02142@gmail.com>
2021-04-02 15:05:23 -0700

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "this is the home page!"
+
+@app.route("/about")
+def about():
+    return "this is the about page"

2d5044ee80888ca645b0eed4cfa56828e434012a
Luis <lflores02142@gmail.com>
2021-04-03 14:15:57 -0700

    installed flask-sqlalchemy to add databases

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegistrationForm
+
+
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'e4f49b0f7a880f50cdda741e13a3e203'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self) -> str:
+        return f"User(username: {self.username}, email: {self.email}, image_file: {self.image_file})"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post(Title: {self.title}, date_posted: {self.date_posted})"
 
 
 posts = [
@@ -29,10 +62,11 @@ def home():
 def about():
     return render_template('about.html', title="about")
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    
+
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == "password":
             flash('You have been logged in !', 'success')
@@ -41,6 +75,7 @@ def login():
             flash('Login unsuccessful. Please check username and password', 'danger')
 
     return render_template('login.html', title="Login", form=form)
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -51,9 +86,6 @@ def register():
         return redirect(url_for('home'))
 
     return render_template('register.html', title="Register", form=form)
-
-
-
 
 
 if __name__ == "__main__":

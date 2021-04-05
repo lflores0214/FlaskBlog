9a9a82064fee3668950d3fd4e2aa1bcaf6d9b267
Luis <lflores02142@gmail.com>
2021-04-05 16:19:18 -0700

    Added an update account form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User, Post
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -31,3 +33,24 @@ class LoginForm(FlaskForm):
                              DataRequired(), Length(min=6, max=24)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already in use')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already in use')
